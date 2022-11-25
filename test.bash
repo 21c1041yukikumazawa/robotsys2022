f6a4811e8be9c99523331083e8122d80a9efa5d7
Yuki Kumazawa <s21c1041nr@s.chibakoudai.jp>
2022-11-25 15:52:14 +0900

    plus + 掛け算

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash  
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
@@ -8,18 +8,18 @@ ng () {
 
 res=0
 
-### I/0 TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+ ### I/0 TEST ###
+ out=$(seq 5 | ./plus)
+ [ "${out}" = 15 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+ ### STRANGE INPUT ###
+ out=$(echo あ | ./plus)
+ [ "$?" = 1 ]      || ng ${LINENO}
+ [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+ out=$(echo | ./plus) #空文字
+ [ "$?" = 1 ]      || ng ${LINENO}
+ [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
