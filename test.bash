8d7cce32c016f1d96cbc8d8bc6e7bf6e5b993b9e
Yuki Kumazawa <s21c1041nr@s.chibakoudai.jp>
2022-11-27 18:50:21 +0900

    test修正

--- test.bash
+++ test.bash
@@ -1,17 +1,5 @@
-#!/bin/bash -xv 
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
-# SPDX-License-Identifier: BSD-3-Clause
-ng () {
-        echo NG at Line $1
-	res=1
-}
-
-res=0
-
- ### I/0 TEST ###
- out=$(seq 5 | ./plus)
- [ "$out" = "a + b 15 a * b 120" ] || ng ${LINENO} 
- #[ "$out" = "a * b 120" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-  exit $res
+# SPDX-License-Identifier: BSD-3-Clause　
+out=$(seq 5 | ./plus)
+[ "${out}" = '15 120' ]
