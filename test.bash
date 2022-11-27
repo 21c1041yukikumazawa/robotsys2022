fbafb51f81c8def8c0167334f05f4dc79d3a0229
Yuki Kumazawa <s21c1041nr@s.chibakoudai.jp>
2022-11-27 19:45:54 +0900

    test修正

--- test.bash
+++ test.bash
@@ -1,5 +1,13 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2022 Kumazawa Yuki
 # SPDX-License-Identifier: BSD-3-Clause　
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
 out=$(seq 5 | ./plus)
-[ "${out}" = '15 120' ]
+[ "${out}" = '15 120' ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK        
+exit $res
